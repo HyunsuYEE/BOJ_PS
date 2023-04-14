52df19a0c4392dcf5ed92b0dbd63b0ad4e1a4033
hyunsu Ye <dpgustn1@unist.ac.kr>
2023-04-14 13:35:48 +0900

    hanoi tower - resursion

---
+++ C++/11729.cc
@@ -0,0 +1,95 @@
+// 11729
+#include <bits/stdc++.h>
+using namespace std;
+
+void moveTo(int first, int middle, int last, int n, int *count, vector<string> *(str_list))
+{
+    if (n == 1)
+    {
+
+        return;
+    }
+    else if (n == 2)
+    {
+        (*str_list).push_back(to_string(first));
+        (*str_list).push_back(" ");
+        (*str_list).push_back(to_string(middle));
+        (*str_list).push_back("\n");
+        (*count)++;
+        (*str_list).push_back(to_string(first));
+        (*str_list).push_back(" ");
+        (*str_list).push_back(to_string(last));
+        (*str_list).push_back("\n");
+        (*count)++;
+        (*str_list).push_back(to_string(middle));
+        (*str_list).push_back(" ");
+        (*str_list).push_back(to_string(last));
+        (*str_list).push_back("\n");
+        (*count)++;
+        return;
+    }
+    //  1. 종착지에 맨 위 블럭을 배치함.
+    // (*str_list).push_back(to_string(first));
+    // (*str_list).push_back(" ");
+    // (*str_list).push_back(to_string(last));
+    // (*str_list).push_back("\n");
+    // (*count)++;
+
+    // //  2. 경유지에 그 다음 블럭 배치함.
+    // (*str_list).push_back(to_string(first));
+    // (*str_list).push_back(" ");
+    // (*str_list).push_back(to_string(middle));
+    // (*str_list).push_back("\n");
+    // (*count)++;
+    // //  3. 경유지에 종착지에 있던 블럭을 배치함.
+    // (*str_list).push_back(to_string(last));
+    // (*str_list).push_back(" ");
+    // (*str_list).push_back(to_string(middle));
+    // (*str_list).push_back("\n");
+    // (*count)++;
+    // //  4. 시작지점에 있는 블럭을 종착지에 배치함.
+    // (*str_list).push_back(to_string(first));
+    // (*str_list).push_back(" ");
+    // (*str_list).push_back(to_string(last));
+    // (*str_list).push_back("\n");
+    // (*count)++;
+    moveTo(first, last, middle, n - 1, count, str_list);
+    (*str_list).push_back(to_string(first));
+    (*str_list).push_back(" ");
+    (*str_list).push_back(to_string(last));
+    (*str_list).push_back("\n");
+    (*count)++;
+    // 재귀 시작: 경유지에 있는 n-1개의 하노이 타워를, first를 거쳐서 last로 가게끔 한다.
+    moveTo(middle, first, last, n - 1, count, (str_list));
+}
+
+void hanoi(int n)
+{
+    int count = 0;
+    int *ptr = &count;
+    vector<string> list;
+    if (n == 1)
+    {
+        cout << 1 << "\n"
+             << "1 3"
+             << "\n";
+        return;
+    }
+    moveTo(1, 2, 3, n, ptr, &list);
+    cout << *ptr << "\n";
+    for (size_t i = 0; i < 4 * (*ptr); i++)
+    {
+        cout << list[i];
+    }
+}
+int main(void)
+{
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    hanoi(n);
+}
